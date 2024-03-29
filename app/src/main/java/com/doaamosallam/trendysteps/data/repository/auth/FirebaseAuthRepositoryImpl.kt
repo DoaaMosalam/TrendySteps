75c0647f1fca853c0529a1b8f94b2c90476f1cd9
Doaa Mosalam <DoaaMosalam9@gmail.com>
2024-03-29 12:28:30 +0200

    Login in with google

--- app/src/main/java/com/doaamosallam/trendysteps/data/repository/auth/FirebaseAuthRepositoryImpl.kt
+++ app/src/main/java/com/doaamosallam/trendysteps/data/repository/auth/FirebaseAuthRepositoryImpl.kt
@@ -1,8 +1,6 @@
 package com.doaamosallam.trendysteps.data.repository.auth
 
-import android.util.Log
 import com.doaamosallam.trendysteps.data.model.Resource
-import com.doaamosallam.trendysteps.ui.auth.fragment.LoginFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.coroutines.flow.Flow
@@ -10,7 +8,8 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
 
 class FirebaseAuthRepositoryImpl(
-    private val auth: FirebaseAuth = FirebaseAuth.getInstance()) :
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+) :
     FirebaseAuthRepository {
 
     override suspend fun loginWithEmailAndPassword(
@@ -31,22 +30,22 @@ class FirebaseAuthRepositoryImpl(
     }
 
     override suspend fun loginWithGoogle(idToken: String): Flow<Resource<String>> = flow {
-            try {
-                emit(Resource.Loading())
-                val credential= GoogleAuthProvider.getCredential(idToken,null)
-                val authResult = auth.signInWithCredential(credential)
-                authResult.result.user.let {    user ->
-                    emit(Resource.Success(user!!.uid))
+        try {
+            emit(Resource.Loading())
+            val credential = GoogleAuthProvider.getCredential(idToken, null)
+            val authResult = auth.signInWithCredential(credential)
+            authResult.result.user.let { user ->
+                emit(Resource.Success(user!!.uid))
 
-                }?:run {
-                    emit(Resource.Error(Exception("User not found")))
-                }
-
-            }catch (e:Exception){
-                emit(Resource.Error(e))
-
+            } ?: run {
+                emit(Resource.Error(Exception("User not found")))
             }
+
+        } catch (e: Exception) {
+            emit(Resource.Error(e))
+
         }
+    }
 
 
     companion object {
--- app/src/main/java/com/doaamosallam/trendysteps/utils/Credential.kt
+++ app/src/main/java/com/doaamosallam/trendysteps/utils/Credential.kt
@@ -1,5 +1,5 @@
 package com.doaamosallam.trendysteps.utils
 
 object Credential {
-    const val REQUEST_CODE_SIGN_IN = 9001
+
 }
